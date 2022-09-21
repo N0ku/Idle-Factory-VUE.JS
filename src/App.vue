d9bbf344de22ba68c2c74cd037528485b4148b4f
N0ku <lucielecouturier2@gmail.com>
2022-09-21 10:32:31 +0200

    init project

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <header>
+    <nav>
+      <ul>
+        <li>
+          <router-link to="/">Home</router-link>
+        </li>
+      </ul>
+    </nav>
+  </header>
+  <router-view></router-view>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+};
 </script>
 
 <style>
