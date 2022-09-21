d05d4ee3546632cbf92d055e6672a488063ed7bc
KillianVendewinkele <killian.vendewinkele@gmail.com>
2022-09-21 15:23:20 +0200

    defining the store

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,27 @@
 import { defineStore } from 'pinia'
 export const useMyStore = defineStore('my', {
-    state: () => {
-
-    },
+    state: () => ({
+        users : [],
+        ressources : [],
+        factories : [],
+        offers : []
+    }),
     actions: {
-
+        async getUsers() {
+            const { data } = await fetchApiProducts()
+            this.users = data
+        },
+        async getRessources() {
+            const { data } = await fetchApiProducts()
+            this.ressources = data
+        },
+        async getFactories() {
+            const { data } = await fetchApiProducts()
+            this.factories = data
+        },
+        async getOffers() {
+            const { data } = await fetchApiProducts()
+            this.offers = data
+        }
     }
 });
