5fac96b8f116c8977cdc19bc6dd978f8319e8030
N0ku <lucielecouturier2@gmail.com>
2022-09-22 15:14:21 +0200

    for checkout

--- src/pages/Factory.vue
+++ src/pages/Factory.vue
@@ -1,14 +1,17 @@
 <template>
   <DisplayRes></DisplayRes>
+  <DisplayUser></DisplayUser>
 </template>
 
 <script>
 import DisplayRes from '../components/DisplayRes.vue'
+import DisplayUser from '../components/DisplayUser.vue'
 
 export default {
   name: "FactoryPage",
   components: {
     DisplayRes,
+    DisplayUser
 }
 };
 </script>
